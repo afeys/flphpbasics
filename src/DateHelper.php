638be225a99337a82034a23de9594f7ab6d374fa
afeys <andy@feys.be>
2024-03-26 10:19:32 +0100

    Added DateHelper.php

---
+++ src/DateHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace flphpbasics;
+
+class DateHelper
+{
+    public static function getWeekDaysForYearMonth($year, $month) {
+        // the first 28 days of the month contain 20 workdays. So only calculate
+        // for the last few days of the month and add 20.
+        $lastday = date("t", mktime(0, 0, 0, $month, 1, $year));
+        $weekdays = 0;
+        for ($day = 29; $day <= $lastday; $day++) {
+            $wd = date("w", mktime(0, 0, 0, $month, $day, $year));
+            if ($wd > 0 && $wd < 6)
+                $weekdays++;
+        }
+        return $weekdays + 20;
+    }
+
+}
--- src/init.php
+++ src/init.php
@@ -4,4 +4,5 @@ require_once(dirname(__FILE__) . '/EncryptionHelper.php');
 require_once(dirname(__FILE__) . '/NumberHelper.php');
 require_once(dirname(__FILE__) . '/StringHelper.php');
 require_once(dirname(__FILE__) . '/UrlHelper.php');
+require_once(dirname(__FILE__) . '/DateHelper.php');
 require_once(dirname(__FILE__) . '/Functions.php');
