09df197041650d6cec9f85a5138eadfaebf71c98
afeys <andy@feys.be>
2024-03-26 15:46:28 +0100

    Added DateHelper.php

--- src/DateHelper.php
+++ src/DateHelper.php
@@ -7,6 +7,7 @@ class DateHelper
     public static function getWeekDaysForYearMonth($year, $month) {
         // the first 28 days of the month contain 20 workdays. So only calculate
         // for the last few days of the month and add 20.
+        $year = intval($year);
         $lastday = date("t", mktime(0, 0, 0, $month, 1, $year));
         $weekdays = 0;
         for ($day = 29; $day <= $lastday; $day++) {
