4d306672ea5552b33790f660b83aac0d7ac6e50a
afeys <andy@feys.be>
2024-04-22 12:01:16 +0200

    Added DateHelper.php

--- src/Functions.php
+++ src/Functions.php
@@ -2,6 +2,10 @@
 // this php file contains various simple helperfunctions
 
 namespace FL;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionProperty;
+
 class Functions {
     static function if($condition, $iftrue = true, $iffalse = true) {
         if ($condition) {
@@ -9,4 +13,19 @@ class Functions {
         }
         return $iffalse;
     }
+
+    /**
+     * @throws ReflectionException
+     */
+    static function hasStaticProperty($className, $propertyName): bool
+    {
+        $reflectionClass = new ReflectionClass($className);
+        foreach ($reflectionClass->getProperties(ReflectionProperty::IS_STATIC) as $property) {
+            if ($property->getName() === $propertyName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
