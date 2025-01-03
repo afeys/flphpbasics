bd2330d77762d453d4bce3cdebb732fd4927dd10
afeys <andy@feys.be>
2025-01-03 15:33:12 +0100

    Added ObjectHelper

--- src/init.php
+++ src/init.php
@@ -5,4 +5,5 @@ require_once(dirname(__FILE__) . '/NumberHelper.php');
 require_once(dirname(__FILE__) . '/StringHelper.php');
 require_once(dirname(__FILE__) . '/UrlHelper.php');
 require_once(dirname(__FILE__) . '/DateHelper.php');
+require_once(dirname(__FILE__) . '/ObjectHelper.php');
 require_once(dirname(__FILE__) . '/Functions.php');
